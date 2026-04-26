834216a76ed3e3c0935635c55cba3330b4d9dd83
Dhruv Sharma <dhruvsharma@Dhruvs-MacBook-Air.local>
2026-04-25 21:49:27 -0600

    Clean up scratch and leftover development files

--- test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('index.html', 'utf8');
-const js = html.split('<script>')[1].split('</script>')[0];
-try {
-  new Function(js);
-  console.log('No syntax errors');
-} catch(e) {
-  console.log('Syntax error:', e);
-}
